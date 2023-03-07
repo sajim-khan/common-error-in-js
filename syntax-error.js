ffccdd2bec7c1fed9656d200b919f59ce75b03cd
sajim-khan <shossain1427@gmail.com>
2023-03-08 00:30:28 +0600

    added type-error

--- syntax-error.js
+++ syntax-error.js
@@ -6,3 +6,15 @@ function bigNum (num) {
         return false;
     }
 }
+
+// another example
+//const sum = a, b => a + b
+const sum =( a, b) => a + b // error first braket 
+const result = sum(3,54 )
+console.log(result);
+
+const student = {
+    name : "Student", // comma na dile error hobe
+    age : 34
+}
+
---
+++ type-error.js
@@ -0,0 +1,13 @@
+// const numb = 5;
+// numb.push(234)
+
+
+// let number ;
+// console.log(number.home);
+
+let numbers = {num: 1, post : 23324} ;
+// console.log(numbers.home.address);
+//console.log(numbers?. home?.address); //result => undefined
+
+const a = 'abul';
+a = 'abul'
