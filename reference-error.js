33b4336a17e5a70d9820326fe99724e234d7ae72
sajim-khan <shossain1427@gmail.com>
2023-03-08 22:51:55 +0600

    added reference error

---
+++ reference-error.js
@@ -0,0 +1,18 @@
+// const a = 'sajim'
+// const b = 'khan'
+// //console.log(w); // error
+
+// console.log(name);
+// const name = 'jdsdsjfkcd'
+// let name = 'jdsdsjfkcd'
+
+
+//ok
+console.log(add(34,43));
+function add (a,b){
+    return a + b;
+}
+
+console.log(add2(34,43));
+const add2 = (a,b) => a + b;  // error
+
